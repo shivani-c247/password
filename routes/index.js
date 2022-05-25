c017b69dbe8026099a34e86d8b2aab6d135365a2
shivani panwar <shivani.p@chapter247.com>
2022-05-25 14:39:52 +0530

    deleted

--- routes/index.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require("express");
-const userOtpRoute = require("./userOtpRoutes");
-const magicLinkRoute = require("./magicLinkRoute");
-const router = Router();
-
-router.use("/api", userOtpRoute);
-router.use("/api",magicLinkRoute);
-module.exports = router;
